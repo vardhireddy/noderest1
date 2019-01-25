d0059f8ea1cae33c471ebfaa03c0174c46c7fc71
vardhireddy <krishna.vardhireddy@gmail.com>
2019-01-25 05:33:01 +0000

    adding the server

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # noderest1
+   41  docker ps
+   42  docker stop 2febb59887ab
+   43  docker rm 2febb59887ab
+   44  docker build -t krishnavardhireddy/node-web-app1 .
+   45  docker images
+   46  docker run -p 49160:8081 -d krishnavardhireddy/node-web-app1
+   47  docker logs 736bd20d59b5b539ba261ada42b766bdc3cb603e8f7ad2fe72275be7a5f735de
+
--- server.js
+++ server.js
@@ -1,16 +1,44 @@
-'use strict';
+var http = require("http");
+var express = require('express');
+var app = express();
+var mysql      = require('mysql');
+var bodyParser = require('body-parser');
 
-const express = require('express');
-
-// Constants
-const PORT = 8081;
-const HOST = '0.0.0.0';
-
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
+//start mysql connection
+var connection = mysql.createConnection({
+  host     : 'localhost', //mysql database host name
+  user     : 'root', //mysql database user name
+  password : '', //mysql database password
+  database : 'test' //mysql database name
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+connection.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected with mysql database...')
+})
+//end mysql connection
+
+//start body-parser configuration
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+//end body-parser configuration
+
+//create app server
+var server = app.listen(8081,  "0.0.0.0", function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+});
+
+//rest api to get all customers
+app.get('/app/employee', function (req, res) {
+   connection.query('select * from employee', function (error, results, fields) {
+	  if (error) throw error;
+	  res.end(JSON.stringify(results));
+	});
+});
