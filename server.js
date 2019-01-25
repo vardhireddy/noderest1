3fd611994bd54d9d0f67720f514bea1b9b05275c
krishna.vardhireddy <krishna.vardhireddy@gmail.com>
2019-01-25 10:13:20 +0000

    remove my sql

--- server.js
+++ server.js
@@ -1,29 +1,7 @@
 var http = require("http");
 var express = require('express');
 var app = express();
-var mysql      = require('mysql');
-var bodyParser = require('body-parser');
 
-//start mysql connection
-var connection = mysql.createConnection({
-  host     : 'localhost', //mysql database host name
-  user     : 'root', //mysql database user name
-  password : 'root', //mysql database password
-  database : 'mysqlemp' //mysql database name
-});
-
-connection.connect(function(err) {
-  if (err) throw err
-  console.log('You are now connected with mysql database...')
-})
-//end mysql connection
-
-//start body-parser configuration
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
-//end body-parser configuration
 
 //create app server
 var server = app.listen(8081,  "0.0.0.0", function () {
@@ -46,8 +24,5 @@ app.get('/app/employee', function (req, res) {
    results.push(emp2)
    results.push(emp3)
    results.push(emp4)
-   //connection.query('select * from employee', function (error, results, fields) {
-	//  if (error) throw error;
-	  res.end(JSON.stringify(results));
-	});
+   res.end(JSON.stringify(results));
 });
